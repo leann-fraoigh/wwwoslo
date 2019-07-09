a29b342023d3618a3a6b9422ebbba902fd7da7d5
Leann Fraoigh <d.myshlennikova@gmail.com>
2019-07-09 13:25:03 +0200

    Improve menu (close after clicking a link)

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,15 +1,32 @@
 var nav = document.querySelector(".nav");
 var navToggler = nav.querySelector(".nav__toggler");
+var menyLinks = document.querySelectorAll(".menu__link");
 
 nav.classList.remove("nav--no-js");
 nav.classList.add("nav--menu-closed");
 
+// Close menu
+ var closeMenu = function() {
+  nav.classList.add("nav--menu-closed");
+  nav.classList.remove("nav--menu-opened"); 
+ }
+
+ // Open menu
+ var openMenu = function() {
+  nav.classList.remove("nav--menu-closed");
+  nav.classList.add("nav--menu-opened");
+ }
+
 navToggler.addEventListener("click", function () {
   if (nav.classList.contains("nav--menu-closed")) {
-    nav.classList.remove("nav--menu-closed");
-    nav.classList.add("nav--menu-opened");
+    openMenu();
   } else {
-    nav.classList.add("nav--menu-closed");
-    nav.classList.remove("nav--menu-opened");    
+    closeMenu();
   }
 })
+
+
+// Add eventListeners to all links
+for (var i = 0; i < menyLinks.length; i++) {
+  menyLinks[i].addEventListener('click', closeMenu);
+}
