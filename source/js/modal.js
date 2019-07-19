9c7f20f63335375d4eb478e9ccdc09c98dedd045
Leann Fraoigh <d.myshlennikova@gmail.com>
2019-07-19 19:29:42 +0200

    Add close button to modals

--- source/js/modal.js
+++ source/js/modal.js
@@ -2,32 +2,33 @@ var Enter = 13;
 var Esc = 27;
 var teamButtons = document.querySelectorAll(".teams__name");
 var overlay = document.querySelector(".overlay");
+var closeButtons = document.querySelectorAll(".modal__close-button");
 
+// Open modal function
+var openModal = function (evt) {
+  evt.preventDefault();
+  evt.target.nextElementSibling.classList.add("modal--opened");
+  overlay.classList.add("overlay--showed");
+}
 
-// Add eventListeners to all modals
+// Close modal function
+var closeModal = function (evt) {
+  evt.preventDefault();
+  document.querySelector('.modal--opened').classList.remove('modal--opened');
+  overlay.classList.remove("overlay--showed");
+}
+
+// Add eventListeners to all preview buttons
 for (var i = 0; i < teamButtons.length; i++) {
   var teamButton = teamButtons[i];
   teamButton.addEventListener("click", function (evt) {
-    teamCliclHandler(evt);
+    openModal(evt);
   });
   teamButton.addEventListener("keydown", function (evt) {
     if (evt.keyCode === Enter) {
-      teamKeydownHandler(evt);
+      openModal(evt);
     }
   });
-};
-
-// Open modal handlers
-var teamCliclHandler = function (evt) {
-  evt.preventDefault();
-  evt.target.nextElementSibling.classList.add("modal--opened");
-  overlay.classList.add("overlay--showed");
-};
-
-var teamKeydownHandler = function (evt) {
-  evt.preventDefault();
-  evt.target.nextElementSibling.classList.add("modal--opened");
-  overlay.classList.add("overlay--showed");
 };
 
 // Close modal handlers
@@ -35,15 +36,26 @@ var teamKeydownHandler = function (evt) {
 window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
     if (document.querySelector('.modal--opened') !== undefined) {
-      evt.preventDefault();
-      document.querySelector('.modal--opened').classList.remove('modal--opened');
-      overlay.classList.remove("overlay--showed");
+      closeModal(evt);
     }
   }
 });
 
+// Click on overlay closes a modal window
 overlay.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  document.querySelector('.modal--opened').classList.remove('modal--opened');
-  overlay.classList.remove("overlay--showed");
+  closeModal(evt);
 });
+
+// Add closeModal handler to all close buttons
+for (var i = 0; i < closeButtons.length; i++) {
+  var closeButton = closeButtons[i];
+  closeButton.addEventListener("click", function (evt) {
+    closeModal(evt);
+  });
+  closeButton.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === Enter) {
+      closeModal(evt);
+    }
+  });
+};
+
